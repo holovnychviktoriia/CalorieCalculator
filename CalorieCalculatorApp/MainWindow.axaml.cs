e24772dd359797ff126ef3b986dd03fb19904fe9
Viktoriia Holovnych <c.holovnych.viktoriia@student.uzhnu.edu.ua>
2026-04-11 21:18:08 +0300

    Day 3: page navigation

--- CalorieCalculatorApp/MainWindow.axaml.cs
+++ CalorieCalculatorApp/MainWindow.axaml.cs
@@ -4,37 +4,40 @@ namespace CalorieCalculatorApp;
 
 public partial class MainWindow : Window
 {
-    private TextBlock? _contentArea;
+    private ContentControl? _mainContent;
     private TextBlock? _statusBar;
 
     public MainWindow()
     {
         InitializeComponent();
-        _contentArea = this.FindControl<TextBlock>("ContentArea");
+        _mainContent = this.FindControl<ContentControl>("MainContent");
         _statusBar = this.FindControl<TextBlock>("StatusBar");
+
+        if (_mainContent != null)
+            _mainContent.Content = new HomePage();
     }
 
     private void BtnHome_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        if (_contentArea != null) _contentArea.Text = "Головна сторінка";
+        if (_mainContent != null) _mainContent.Content = new HomePage();
         if (_statusBar != null) _statusBar.Text = "Розділ: Головна";
     }
 
     private void BtnJournal_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        if (_contentArea != null) _contentArea.Text = "Журнал харчування";
+        if (_mainContent != null) _mainContent.Content = new JournalPage();
         if (_statusBar != null) _statusBar.Text = "Розділ: Журнал харчування";
     }
 
     private void BtnFood_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        if (_contentArea != null) _contentArea.Text = "База продуктів";
+        if (_mainContent != null) _mainContent.Content = new FoodPage();
         if (_statusBar != null) _statusBar.Text = "Розділ: База продуктів";
     }
 
     private void BtnSettings_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        if (_contentArea != null) _contentArea.Text = "Налаштування";
+        if (_mainContent != null) _mainContent.Content = new SettingsPage();
         if (_statusBar != null) _statusBar.Text = "Розділ: Налаштування";
     }
 }
---
+++ CalorieCalculatorApp/Views/FoodPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CalorieCalculatorApp;
+
+public partial class FoodPage : UserControl
+{
+    public FoodPage()
+    {
+        InitializeComponent();
+    }
+}
